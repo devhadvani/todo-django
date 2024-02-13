8e91d472066f458526a4cb812395f895ab2d7a00
devhadvani <65004285+devhadvani@users.noreply.github.com>
2024-02-13 09:36:47 +0530

    addded update and delete funtion

--- app/views.py
+++ app/views.py
@@ -1,17 +1,48 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
+
 # Create your views here.
 def home(request):
     fm = TodoForm()
     data = Todo.objects.all()
-    if request.method == "GET":
-        fm = TodoForm(request.GET)
+    if request.method == "POST":
+        fm = TodoForm(request.POST)
         if fm.is_valid():
             fm.save()
             print("yes")
+            messages.success(request,"insterted successfuly")
+            fm = TodoForm()
+
         # name = request.GET['name']
         # user = Todo(name=name)
         # user.save()
     return render(request,"home.html",{"data":data,"form":fm})
+
+def update(request,id):
+    todo = Todo.objects.get(id=id)
+    if request.method == "POST":
+        fm = TodoForm(request.POST,instance=todo)
+        if fm.is_valid():
+            fm.save()
+            return redirect('home')
+    else:
+        form =  TodoForm(instance=todo)
+    return render(request,'update.html',{"form":form})
+
+def delete(request,id):
+    # if request.method =="GET":
+    record = Todo.objects.get(id=id)
+    record.delete()
+    messages.success(request,"deleted successfuly")
+
+    return redirect('home')
+
+def done(request,id):
+    record = Todo.objects.get(id=id)
+    record.complete = True
+    record.save()
+    return redirect('home')
+    # return reb
